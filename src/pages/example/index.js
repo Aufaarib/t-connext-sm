db5d03da6efe56d6024deab50162d89adb4966de
Aufaarib <aufa@nuncorp.id>
2023-06-05 10:44:06 +0700

    first commit

---
+++ src/pages/example/index.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { addOne, subOne, addSome, subSome, reset } from '../slice'
+
+const example = () => {
+  const dispatch = useDispatch()
+  const count = useSelector(state => state.counter.count)
+
+  return (
+    <div className='App'>
+      <p>Count is: {count}</p>
+
+      <div>
+        <button onClick={() => dispatch(addOne())}>Add 1</button>
+        <button onClick={() => dispatch(subOne())}>Decrease 1</button>
+        <button onClick={() => dispatch(addSome(10))}>Add 10</button>
+        <button onClick={() => dispatch(subSome(10))}>Decrease 10</button>
+        <button onClick={() => dispatch(reset())}>Reset count</button>
+      </div>
+    </div>
+  )
+}
+
+export default example
